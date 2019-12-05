de22ad1cbe0c85fd918dbd81ce48cd8a3ccda356
Glitch (ifx5-lalaland-1) <none>
2019-12-05 01:30:16 +0000

    👄💁 Checkpoint ./script.js:5643072/1731

--- script.js
+++ script.js
@@ -4,25 +4,52 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-var mymap = L.map('mapid').setView([34.0522, -118.2437], 13);
 
-var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+// Leaflet maps //
+// --------------------------------------------------------------- // 
+
+mapOptions = {
+  'preferCanvas': true,
+  'zoomControl': false,
+  'renderer': L.Canvas
+ }
+
+var mymap = L.map('mapid', mapOptions).setView([34.0522, -118.2437], 13);
+
+// var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+// 	subdomains: 'abcd',
+// 	maxZoom: 19
+// }).addTo(mymap);
+
+var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
 	maxZoom: 19
 }).addTo(mymap);
 
+
+// WAYPOINTS //
+// --------------------------------------------------------------- // 
+// http://imakewebthings.com/waypoints/guides/getting-started/
+point_home = L.latLng(-118.2437, );
+point_1 = L.latLng(50.5, 30.5);
+
 var waypoint = new Waypoint({
   element: document.getElementById('scrollstory'),
   handler: function(direction) {
+    mymap.panTo(point_1);
     console.log('Scrolled to ' + direction);
   }
-})
+});
 
 var waypoint2 = new Waypoint({
   element: document.getElementsByClassName("landmark"),
   handler: function(direction) {
+    mymap.panTo(point_home);
     console.log('Scroll handler for down' + this);
   }
-})
+});
 
+
+// mymap.panTo(point_1);
