002b1e4da5a361e100858e9dc5b1e8af9f233c1f
Glitch (ifx5-lalaland-1) <none>
2019-12-08 23:35:28 +0000

    👧🍧 Checkpoint ./index.html:5643072/63735 ./script.js:5643072/1248 ./style.css:5643072/18

--- script.js
+++ script.js
@@ -69,51 +69,8 @@ make_waypoint = (selector, triggerpoint, offsety, callbacky= x=>{}) => {
 };
 
 make_waypoint("#introduction", point_home, -20);
-make_waypoint("#hollywood", point_hollywood, 50,x => {player.seekTo(3); player.playVideo(); return console.log('lolol')});
+make_waypoint("#hollywood", point_hollywood, 50,x => { console.log('lolol')});
 make_waypoint("#burbank", point_burbank, 50);
 make_waypoint("#appendix", point_nyc, 900);
 make_waypoint("#koreatown", point_koreatown, 50);
-
-// mymap.panTo(point_1);
-
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement("script");
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName("script")[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player("player", {
-    height: "50",
-    width: "500",
-    videoId: "5wBTdfAkqGU",
-    events: {
-      onReady: onPlayerReady,
-      onStateChange: onPlayerStateChange
-    }
-  });
-}
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  // event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo();
-}
 
