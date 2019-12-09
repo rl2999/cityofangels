98e049deeac701dfbdde588f53a1f69964cee517
Glitch (ifx5-lalaland-1) <none>
2019-12-09 00:11:28 +0000

    🏆🌅 Checkpoint ./style.css:5643072/1328 ./index.html:5643072/106 ./script.js:5643072/805

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ console.log("hi");
 // Leaflet maps //
 // --------------------------------------------------------------- //
 
-mapOptions = {
+var mapOptions = {
   preferCanvas: true,
   zoomControl: false,
   renderer: L.Canvas,
   dragging: false
 };
 
-var mymap = L.map("mapid", mapOptions).setView([34.0522, -118.2437], 13);
+var point_home = L.latLng(34.0522, -118.2437);
+
+var mymap = L.map("mapid", mapOptions).setView(point_home, 13);
 
 // var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 // 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
@@ -39,21 +41,20 @@ var CartoDB_Positron = L.tileLayer(
 // Declare our zoom points on the map
 // Make them with geojson.io but note that its flipped
 // http://geojson.io/#map=19/40.80805/-73.96041
-point_home = L.latLng(34.0522, -118.2437);
-point_1 = L.latLng(50.5, 30.5);
-point_hollywood = L.latLng(34.1016774615434, -118.330135345459);
-point_nyc = L.latLng(40.80807627279606, -73.96046251058578);
-point_burbank = L.latLng(34.18539, -118.364295);
-point_koreatown = L.latLng(34.028762179464465, -118.26476454734802);
+var point_venice = L.latLng(33.99539435637889, -118.46677780151366);  
+var point_hollywood = L.latLng(34.1016774615434, -118.330135345459);
+var point_nyc = L.latLng(40.80807627279606, -73.96046251058578);
+var point_burbank = L.latLng(34.18539, -118.364295);
+var point_koreatown = L.latLng(34.028762179464465, -118.26476454734802);
 
 // This is a callback function
 // it changes locations for us
-zoomToLocation = (point, zoom) => {
+const zoomToLocation = (point, zoom) => {
   mymap.flyTo(point, zoom, { animate: true, duration: 2, easeLinearity: 0.1 });
   // mymap.setZoom(zoom);
 };
 
-make_waypoint = (selector, triggerpoint, offsety, callbacky= x=>{}) => {
+const make_waypoint = (selector, triggerpoint, offsety, callbacky= x=>{}) => {
   new Waypoint({
     element: document.querySelector(selector),
     handler: function(direction) {
@@ -69,8 +70,9 @@ make_waypoint = (selector, triggerpoint, offsety, callbacky= x=>{}) => {
 };
 
 make_waypoint("#introduction", point_home, -20);
-make_waypoint("#hollywood", point_hollywood, 50,x => { console.log('lolol')});
-make_waypoint("#burbank", point_burbank, 50);
-make_waypoint("#appendix", point_nyc, 900);
 make_waypoint("#koreatown", point_koreatown, 50);
+make_waypoint("#hollywood", point_hollywood, 50);
+make_waypoint("#venice", point_venice, 50,x => { console.log('lolol')});
+// make_waypoint("#burbank", point_burbank, 50);
+make_waypoint("#appendix", point_nyc, 900);
 
