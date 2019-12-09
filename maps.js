6f1853cda30a585acd0d0001e3128edf4cec1155
Glitch (ifx5-lalaland-1) <none>
2019-12-09 17:53:54 +0000

    🎃🐞 Checkpoint ./index.html:5643072/46435 ./script.js:5643072/1742 ./maps.js:5643072/1182 ./style.css:5643072/55

--- maps.js
+++ maps.js
@@ -1,6 +1,6 @@
 // neighborhoodBounds = postData();
 
-var url2 = "https://cdn.glitch.com/48204e47-9ee8-4828-954c-c495450f3d3d%2FLA-Neighborhoods_crs.geojson?v=1575860761151"
+var url2 = "https://cdn.glitch.com/48204e47-9ee8-4828-954c-c495450f3d3d%2FLA-Neighborhoods_crs.geojson"
 fetch(url2)
   .then(function (response) {
     // Read data as JSON
@@ -11,3 +11,33 @@ fetch(url2)
     L.geoJson(data).addTo(mainMap);
   });
 
+
+var url_neighborhood_bounds = "https://s3-us-west-2.amazonaws.com/boundaries.latimes.com/archive/1.0/boundary-set/lapd-bureaus.geojson";
+
+// "https://raw.githubusercontent.com/johan/world.geo.json/master/countries/USA/CA/Los%20Angeles.geo.json";
+// "https://cdn.glitch.com/48204e47-9ee8-4828-954c-c495450f3d3d%2FLA-Neighborhoods_crs.geojson?v=1575860761151";
+// "https://cdn.glitch.com/48204e47-9ee8-4828-954c-c495450f3d3d%2FLA-Neighborhoods.geojson?v=1575855297854"
+
+// var neighborhoodBounds = fetch(url_neighborhood_bounds)
+//   .then(
+//     function(response) {
+//       if (response.status !== 200) {
+//         console.log('Looks like there was a problem. Status Code: ' +
+//           response.status);
+//         return;
+//       }
+
+//       // Examine the text in the response
+//       response.json().then(function(data) {
+//         console.log(data);
+//         return data;
+//       });
+//     }
+//   )
+//   .catch(function(err) {
+//     console.log('Fetch Error :-S', err);
+//   });
+
+
+// data_neighborhoods = $.getJSON(url_neighborhood_bounds).responseJSON;
+// neighborhoods = L.geoJson(geo).addTo(mymap); //Adds the layer to the map.
