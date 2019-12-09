e813a9a178b8d4dd0e05583a9635eb64f664a394
Glitch (ifx5-lalaland-1) <none>
2019-12-09 18:59:17 +0000

    ⛱🏗 Checkpoint ./maps.js:5643072/8098

--- maps.js
+++ maps.js
@@ -1,30 +1,37 @@
 // neighborhoodBounds = postData();
 
-var url2 = "https://cdn.glitch.com/48204e47-9ee8-4828-954c-c495450f3d3d%2FLA-Neighborhoods_crs.geojson"
-  try{
-   
-fetch(url2)
-  .then(function (response) {
-    // Read data as JSON
-    return response.json();
-  })
-  .then(function (data) {
-    // Add data to the map
-  var myLayer = L.geoJSON().addTo(mainMap);
-  console.log(data);
-  myLayer.addData(data);
-    // L.geoJSON(data).addTo(mainMap);
-  });
-    
-   }
-   catch {
+// https://leafletjs.com/reference-1.6.0.html#path-option
+geojsonStyle = {
+  color: "#52cc00",
+  weight: 1.5,
+  fillOpacity: 0,
+  dashArray: "10",
+  opacity: 1
+};
 
-      // # nothing
-    }
+var url2 =
+  "https://cdn.glitch.com/48204e47-9ee8-4828-954c-c495450f3d3d%2FLA-Neighborhoods_crs.geojson";
+try {
+  fetch(url2)
+    .then(function(response) {
+      // Read data as JSON
+      return response.json();
+    })
+    .then(function(data) {
+      // Add data to the map
 
+      var myLayer = L.geoJSON(data, geojsonStyle).addTo(mainMap);
+      console.log(data);
+      // myLayer.addData(data);
 
+      // L.geoJSON(data).addTo(mainMap);
+    });
+} catch {
+  // # nothing
+}
 
-var url_neighborhood_bounds = "https://s3-us-west-2.amazonaws.com/boundaries.latimes.com/archive/1.0/boundary-set/lapd-bureaus.geojson";
+var url_neighborhood_bounds =
+  "https://s3-us-west-2.amazonaws.com/boundaries.latimes.com/archive/1.0/boundary-set/lapd-bureaus.geojson";
 
 // "https://raw.githubusercontent.com/johan/world.geo.json/master/countries/USA/CA/Los%20Angeles.geo.json";
 // "https://cdn.glitch.com/48204e47-9ee8-4828-954c-c495450f3d3d%2FLA-Neighborhoods_crs.geojson?v=1575860761151";
@@ -49,7 +56,6 @@ var url_neighborhood_bounds = "https://s3-us-west-2.amazonaws.com/boundaries.lat
 //   .catch(function(err) {
 //     console.log('Fetch Error :-S', err);
 //   });
-
 
 // data_neighborhoods = $.getJSON(url_neighborhood_bounds).responseJSON;
 // neighborhoods = L.geoJson(geo).addTo(mymap); //Adds the layer to the map.
