e5bdb4f52f70a5e3cf20e7454912a19144268ec0
Glitch (homelessness-la) <none>
2019-12-16 20:11:32 +0000

    🎺🏩 Checkpoint ./index.html:5643072/150 ./style.css:5643072/247 ./plots.js:5643072/1202

--- plots.js
+++ plots.js
@@ -8,35 +8,40 @@
 const plotWidth = 400;
 const plotHeight = 400;
 
-  const globalPlotConfig = {
+const globalPlotConfig = {
   autosize: {
-      resize: true,
-      type: "fit"
-    },
-    width: "container",
-    height: "container",
-    axis: {
-      labelFont: "monospace",
-      titleFont: "Courier",
-    },
-    title: {
-      font: "Courier"
-    },
-    background: "rgba(0,0,0,0)",
-    mark: {
-      fill: "#FF5A5F"
-    }
-  };
+    resize: true,
+    type: "fit"
+  },
+  width: "container",
+  height: "container",
+  axis: {
+    labelFont: "monospace",
+    titleFont: "Courier",
+  },
+  title: {
+    font: "Courier"
+  },
+  background: "rgba(0,0,0,0)",
+  mark: {
+    fill: "#FF5A5F"
+  }
+};
 
 const makePlotRentalType = url => {
   var jsondata = {
     width: "container",
     height: "container",
-    autosize : {resize: true, type: "fit"},
+    autosize: {
+      resize: true,
+      type: "fit"
+    },
     title: "Distribution of room types",
     description: "Distribution of room types",
     $schema: "https://vega.github.io/schema/vega-lite/v4.json",
-    data: { url: url },
+    data: {
+      url: url
+    },
     mark: "bar",
     encoding: {
       y: {
@@ -55,11 +60,14 @@ const makePlotRentalType = url => {
 
 const makeOverviewScatter = url => {
   var jsondata = {
-    width: "container",
+    autosize: {
+      resize: false,
+      type: "fit"
+    },
     height: "container",
+    width: "container",
     $schema: "https://vega.github.io/schema/vega-lite/v4.json",
-    title:
-      "Density of Airbnb rentals vs. homeless encampments across all neighborhoods",
+    title: "Density of Airbnb rentals vs. homeless encampments across all neighborhoods",
     data: {
       url: url
     },
@@ -67,20 +75,28 @@ const makeOverviewScatter = url => {
       x: {
         field: "density_airbnb",
         type: "quantitative",
-        scale: { domain: [0, 1.5] }
+        scale: {
+          domain: [0, 1.5]
+        }
       },
       y: {
         field: "density_homeless",
         type: "quantitative",
-        scale: { domain: [0, 2] }
+        scale: {
+          domain: [0, 2]
+        }
       },
-      tooltip: [
-        { field: "density_airbnb", type: "quantitative" },
-        { field: "density_homeless", type: "quantitative" }
+      tooltip: [{
+          field: "density_airbnb",
+          type: "quantitative"
+        },
+        {
+          field: "density_homeless",
+          type: "quantitative"
+        }
       ]
     },
-    layer: [
-      {
+    layer: [{
         selection: {
           grid: {
             type: "interval",
@@ -92,9 +108,16 @@ const makeOverviewScatter = url => {
         fill: "#0b03fc"
       },
       {
-        mark: { type: "text", baseline: "middle", dx: 30 },
+        mark: {
+          type: "text",
+          baseline: "middle",
+          dx: 30
+        },
         encoding: {
-          text: { field: "neighbourhood", type: "nominal" },
+          text: {
+            field: "neighbourhood",
+            type: "nominal"
+          },
           color: {
             value: "black"
           }
@@ -115,28 +138,48 @@ const makePricePlot = dataURL => {
     data: {
       url: dataURL
     },
-    transform: [{ filter: "datum.price < 6000" }],
-    layer: [
-      {
+    transform: [{
+      filter: "datum.price < 6000"
+    }],
+    layer: [{
         mark: "bar",
-        selection: { grid: { type: "interval", bind: "scales" } },
+        selection: {
+          grid: {
+            type: "interval",
+            bind: "scales"
+          }
+        },
         encoding: {
-          tooltip: [
-            { field: "__count" },
-            { field: "price", type: "quantitative" }
+          tooltip: [{
+              field: "__count"
+            },
+            {
+              field: "price",
+              type: "quantitative"
+            }
           ],
           x: {
             bin: false,
             field: "price",
             type: "quantitative",
-            axis: { grid: false, labelFont: "Courier" },
-            scale: { domain: [0, 365] }
+            axis: {
+              grid: false,
+              labelFont: "Courier"
+            },
+            scale: {
+              domain: [0, 365]
+            }
           },
           y: {
             aggregate: "count",
             type: "quantitative",
-            axis: { grid: false, labelFont: "Courier" },
-            scale: { domain: [0, 450] }
+            axis: {
+              grid: false,
+              labelFont: "Courier"
+            },
+            scale: {
+              domain: [0, 450]
+            }
           }
         }
       },
@@ -148,8 +191,12 @@ const makePricePlot = dataURL => {
             field: "price",
             type: "quantitative"
           },
-          color: { value: "blue" },
-          size: { value: 2 }
+          color: {
+            value: "blue"
+          },
+          size: {
+            value: 2
+          }
         }
       }
     ],
@@ -164,35 +211,61 @@ const makeMiniNights = dataURL => {
   var plotData = {
     title: "Frequency of rental units by minimum_nights range",
     $schema: "https://vega.github.io/schema/vega-lite/v4.json",
-    autosize: { resize: true, type: "fit" },
+    autosize: {
+      resize: true,
+      type: "fit"
+    },
+    width: "container",
     data: {
       url: dataURL
     },
-
-    view: { fill: "white", opacity: 0 },
-    transform: [{ filter: "datum.minimum_nights < 6000" }],
-    layer: [
-      {
+    view: {
+      fill: "white",
+      opacity: 0
+    },
+    transform: [{
+      filter: "datum.minimum_nights < 6000"
+    }],
+    layer: [{
         mark: "bar",
-        selection: { grid: { type: "interval", bind: "scales" } },
+        selection: {
+          grid: {
+            type: "interval",
+            bind: "scales"
+          }
+        },
         background: "rgba(0,0,0,0)",
         encoding: {
-          tooltip: [
-            { field: "__count" },
-            { field: "minimum_nights", type: "quantitative" }
+          tooltip: [{
+              field: "__count"
+            },
+            {
+              field: "minimum_nights",
+              type: "quantitative"
+            }
           ],
           x: {
             bin: false,
             field: "minimum_nights",
             type: "quantitative",
-            axis: { grid: false, labelFont: "Courier" },
-            scale: { domain: [0, 365] }
+            axis: {
+              grid: false,
+              labelFont: "Courier"
+            },
+            scale: {
+              domain: [0, 365]
+            }
           },
           y: {
             aggregate: "count",
             type: "quantitative",
-            axis: { grid: false, labelFont: "Courier" },
-            scale: { domain: [0, 450] }
+            axis: {
+              grid: false,
+              labelFont: "Courier"
+            },
+            scale: {
+              domain: [0, 450]
+            }
           }
         }
       },
@@ -204,8 +277,12 @@ const makeMiniNights = dataURL => {
             field: "minimum_nights",
             type: "quantitative"
           },
-          color: { value: "blue" },
-          size: { value: 2 }
+          color: {
+            value: "blue"
+          },
+          size: {
+            value: 2
+          }
         }
       }
     ],
@@ -245,18 +322,18 @@ renderAllPlotsForArea(
 
 // BOKEH //
 // --------------------------------------------------------------- //
-(function() {
-  var fn = function() {
-    Bokeh.safely(function() {
-      (function(root) {
+(function () {
+  var fn = function () {
+    Bokeh.safely(function () {
+      (function (root) {
         function embed_document(root) {
           var docs_json = document.getElementById("9718").textContent;
-          var render_items = [
-            {
-              docid: "85dad36b-030e-45ff-90d6-cbaf4cfe6a36",
-              roots: { "9203": "eb82953e-61c9-4b88-b3dd-a30aa64654c4" }
+          var render_items = [{
+            docid: "85dad36b-030e-45ff-90d6-cbaf4cfe6a36",
+            roots: {
+              "9203": "eb82953e-61c9-4b88-b3dd-a30aa64654c4"
             }
-          ];
+          }];
           root.Bokeh.embed.embed_items(docs_json, render_items);
         }
         if (root.Bokeh !== undefined) {
@@ -264,7 +341,7 @@ renderAllPlotsForArea(
         } else {
           var attempts = 0;
           var timer = setInterval(
-            function(root) {
+            function (root) {
               if (root.Bokeh !== undefined) {
                 embed_document(root);
                 clearInterval(timer);
