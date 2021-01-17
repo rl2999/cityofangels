0fb546bf81c6357d75191f16d5fd7044e5b15ad8
Ri <ri.le@upstatement.com>
2021-01-17 15:04:36 -0500

    css refactored

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './styles/main.css';
+import './styles/main.scss';
 // Web component
 import LandmarkAnnotation from './components/LandmarkAnnotation';
 import LandmarkNeighborhood from './components/LandmarkNeighborhood';
